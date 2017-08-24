0699a412c4782e78b2f3855a2a047fc49df998d4
YJC(openopentw) <openopentw@gmail.com>
2017-08-24 19:17:16 +0800

    use smaller data & delete batchnormalization and another dense
    has significant improvement on training data's accuracy

--- 2.py
+++ 2.py
@@ -31,7 +31,7 @@ EMBD_DIM = 400
 # load training data
 with open(data_path, 'r', encoding='utf8') as f:
     text_data = f.read().splitlines()
-text_data = text_data[ : len(text_data) // 2]
+text_data = text_data[ : 5000]
 print('Found {} sentences.'.format(len(text_data)))
 
 # generate tokenizer for all data (q_train + a_train)
@@ -64,28 +64,6 @@ for _ in range(n_wrong_ans):
 
 # generate sequences
 sequences = tokenizer.texts_to_sequences(text_data)
-
-# generate another one wrong answer
-# ans = []
-# q_sequences = []
-# a_sequences = []
-# rand_ans = np.random.randint(2, size=len(sequences))
-# for i,_ in enumerate(text_data):
-#     if i < 1 or not sequences[i-1] or not sequences[i]:
-#         continue
-#     q_sequences += [ sequences[i-1] ]
-#     q_sequences += [ sequences[i-1] ]
-#     if rand_ans[i] == 0:
-#         ans += [1]
-#         a_sequences += [ sequences[i] ]
-#         ans += [0]
-#         a_sequences += [ sequences[fake_ans_id[i]] ]
-#     else:
-#         ans += [0]
-#         a_sequences += [ sequences[fake_ans_id[i]] ]
-#         ans += [1]
-#         a_sequences += [ sequences[i] ]
-# print('Finish generating questions and answers.')
 
 # concate 3 together & generate other 5 wrong answer
 q_sequences = []
@@ -153,20 +131,12 @@ def generate_model(q_shape, a_shape):
     q_vec = GRU(200, activation='elu', dropout=0.3)(q_vec)
     q_vec = Dropout(0.5)(q_vec)
     q_vec = Dense(100, activation='elu')(q_vec)
-    q_vec = BatchNormalization()(q_vec)
-    q_vec = Dropout(0.5)(q_vec)
-    q_vec = Dense(50, activation='elu')(q_vec)
-    q_vec = BatchNormalization()(q_vec)
 
     a_input = Input(shape=(a_shape,))
     a_vec = Embedding(num_words, EMBD_DIM, weights=[embedding_matrix], trainable=False)(a_input)
     a_vec = GRU(200, activation='elu', dropout=0.3)(a_vec)
     a_vec = Dropout(0.5)(a_vec)
     a_vec = Dense(100, activation='elu')(a_vec)
-    a_vec = BatchNormalization()(a_vec)
-    a_vec = Dropout(0.5)(a_vec)
-    a_vec = Dense(50, activation='elu')(a_vec)
-    a_vec = BatchNormalization()(a_vec)
 
     # use cosine similarity
     # see here: https://github.com/fchollet/keras/issues/2672#issuecomment-218188051
@@ -181,4 +151,4 @@ def generate_model(q_shape, a_shape):
 model = generate_model(q_train.shape[1], a_train.shape[1])
 
 model.compile(loss='binary_crossentropy', optimizer='sgd', metrics=['accuracy'])
-model.fit([q_train, a_train], ans, epochs=10, batch_size=100, validation_split=0.1)
+model.fit([q_train, a_train], ans, epochs=500, validation_split=0.1)

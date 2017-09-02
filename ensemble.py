98807f120515b98c64128f1bbf873bb7c18a00d0
YJC <openopentw@gmail.com>
2017-09-02 20:18:36 +0800

    add `output/8.csv` & ensembles

---
+++ ensemble.py
@@ -0,0 +1,38 @@
+from os import path
+
+import numpy as np
+
+# parameters
+output_IDs = [
+    8,
+    9,
+    10,
+]
+
+ans_path = './subm/'
+
+# generate ans_path
+ans_filename = str(output_IDs[0])
+for ID in output_IDs[1:]:
+    ans_filename += '_' + str(ID)
+ans_filename += '.csv'
+ans_path = path.join(ans_path, ans_filename)
+print('print answer to: {}'.format(ans_path))
+
+# import output files
+outputs = np.zeros((len(output_IDs), 500, 6))
+for i,ID in enumerate(output_IDs):
+    output_path = path.join('output', str(ID) + '.csv')
+    print(output_path)
+    outputs[i] = np.genfromtxt(output_path, delimiter=',')
+
+# get the answer from outputs
+# output = outputs.sum(axis=0)
+output = 2 * outputs[0] + outputs[1] + outputs[2]
+ans = np.argmax(output, axis=1)
+
+# print ans out
+with open(ans_path, 'w') as f:
+    print('id,ans', file=f)
+    for i, a in enumerate(ans):
+        print('{},{}'.format(i+1, a), file=f)
